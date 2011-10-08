57ff8e6c46444e452b2367c650f9732b701e4f82
Waldyr Felix <waldyrfelix@gmail.com>
2011-10-08 17:03:33 -0300

    codigo fonte inicial da implmentacao do DCT

---
+++ DCTAlgorithms/Stego.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DCTAlgorithms
+{
+    public class Stego
+    {
+        internal double[,] calculateDCTForBlock(byte[,] originalBlock)
+        {
+            double[,] dctBlock = new double[8,8];
+
+            for (int u = 0; u < originalBlock.GetLength(0); u++)
+            {
+                for (int v = 0; v < originalBlock.GetLength(1); v++)
+                {
+                    dctBlock[u, v] = calculateDCTForPixel(originalBlock, u, v);
+                }
+            }
+
+            return dctBlock;
+        }
+
+        internal double calculateCosine(int stegoCoord, int coord)
+        {
+            return Math.Cos((2*coord + 1)*stegoCoord*Math.PI/16.0);
+        }
+
+        internal double calculateIteration(byte[,] originalBlock, int x, int y, int u, int v)
+        {
+            return originalBlock[x, y]*calculateCosine(u, x)*calculateCosine(v, y);
+        }
+
+        internal double calculateSum(byte[,] originalBlock, int u, int v)
+        {
+            double sum = 0;
+            for (int x = 0; x < originalBlock.GetLength(0); x++)
+            {
+                for (int y = 0; y < originalBlock.GetLength(1); y++)
+                {
+                    sum += calculateIteration(originalBlock, x, y, u, v);
+                }
+            }
+
+            return sum;
+        }
+
+        internal double calculateDCTForPixel(byte[,] originalBlock, int u, int v)
+        {
+            return calculateWeight(u)*calculateWeight(v)/4*calculateSum(originalBlock, u, v);
+        }
+
+        internal double calculateWeight(int coord)
+        {
+            if (coord == 0)
+            {
+                return 1/Math.Sqrt(2);
+            }
+            return 1;
+        }
+    }
+}
---
+++ DCTAlgorithms/StegoTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DCTAlgorithms
+{
+    [TestClass]
+    public class StegoTest
+    {
+        private const int x = 1000;
+        private const int y = 500;
+        private Stego stego;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            stego = new Stego();
+
+            Console.WriteLine();
+            Console.WriteLine("Coords= ({0},{1})", x, y);
+            Console.WriteLine();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++");
+        }
+
+        [TestMethod]
+        public void Calculate_DCT_for_block_8x8()
+        {
+            byte[,] block = new byte[8, 8];
+
+            string path = @"C:\Users\waldyr\Documents\Visual Studio 2010\Projects\DCTAlgorithms\DCTAlgorithms\ShangHai.jpg";
+            using (var image = new Bitmap(path))
+            {
+                BitmapData blockData = image.LockBits(new Rectangle(1000, 500, 8, 8), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+                unsafe
+                {
+                    byte* imagePointer = (byte*)blockData.Scan0;
+
+                    for (int i = 0; i < blockData.Height; i++)
+                    {
+                        for (int j = 0; j < blockData.Width; j++)
+                        {
+                            block[j, i] = (byte)((imagePointer[0] + imagePointer[1] + imagePointer[2]) / 3);
+                            imagePointer += 3;
+                        }
+                        imagePointer += 3;
+                    }
+                }
+                image.UnlockBits(blockData);
+            }
+
+            var stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+            double[,] dctblock = stego.calculateDCTForBlock(block);
+            stopwatch.Stop();
+
+
+            printMatrixOnConsole("Original block", block);
+            printMatrixOnConsole("DCT block", dctblock);
+            Console.WriteLine("Spended time: "+ stopwatch.Elapsed);
+
+        }
+
+
+        private void printMatrixOnConsole(string name, double[,] matrix)
+        {
+            Console.WriteLine("Print matrix " + name);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(( (int) matrix[i, j]).ToString().PadLeft(4, ' '));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        private void printMatrixOnConsole(string name, byte[,] matrix)
+        {
+            Console.WriteLine("Print matrix " + name);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(( (int) matrix[i, j]).ToString().PadLeft(4, ' '));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
